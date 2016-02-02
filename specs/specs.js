5d185cfbe2b944a8689fe2887b46c36eb21a7267
Kyle Derrick <kderrick1988@gmail.com>
2016-02-01 18:52:38 -0800

    Site function works, still not passing specs

--- specs/specs.js
+++ specs/specs.js
@@ -1,26 +1,22 @@
 describe('Pizza', function () {
   it("Creates a new pizza with the given properties", function () {
-    var testPizza = new Pizza("Large", "[Cheese]");
-    expect(testPizza.pizzaSize).to.be.equal("Large");
-    expect(testPizza.pizzaToppings).to.be.eql("[Cheese]");
+    var testPizza = new Pizza("Large", ["Cheese"]);
+    expect(testPizza.pizzaSize).to.equal("Large");
+    expect(testPizza.pizzaToppings).to.eql(["Cheese"]);
   });
 
   it("adds the sizeCost method to all Pizzas", function() {
-    var testPizza = new Pizza("Large", "[Cheese]");
-    expect(testPizza.sizeCost("Large")).to.be.equal(2);
+    var testPizza = new Pizza("Large", ["Cheese"]);
+    expect(testPizza.sizeCost("Large")).to.equal(2);
   });
 
   it("adds the toppingsCost method to all Pizzas", function () {
-    var testPizza = new Pizza("Large","[Cheese]");
-    expect(testPizza.toppingsCost("[Cheese]")).to.be.equal(11);
-    expect(testPizza.toppingsCost("[Cheese, Onions]")).to.be.equal(12);
-    expect(testPizza.toppingsCost("[Cheese, Onions, Pepperoni]")).to.be.equal(13);
-    expect(testPizza.toppingsCost("[Cheese, Onions, Pepperoni, Sausage]")).to.be.equal(14);
-    expect(testPizza.toppingsCost("[Cheese, Onions, Pepperoni, Sausage, Olives]")).to.be.equal(15);
+    var testPizza = new Pizza("Large",["Cheese"]);
+    expect(testPizza.toppingsCost(["Cheese"]).to.equal(11));
   });
 
   it("adds the totalPizzaCost method to all Pizzas", function () {
-    var testPizza = new Pizza("Large" , "[Cheese]");
-    expect(testPizza.totalPizzaCost()).to.be.equal("The cost of your pizza will be" + " " + 13 + " dollars.");
+    var testPizza = new Pizza("Large" , ["Cheese"]);
+    expect(testPizza.totalPizzaCost()).to.equal("The cost of your pizza will be" + " " + 13 + " dollars.");
   });
 });
